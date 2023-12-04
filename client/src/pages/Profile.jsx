2bd6643b4436f87b066cac36a68c1058b637df4a
Sahak <eversuperskinny@gmail.com>
2023-12-04 21:29:49 +0400

    create update listing api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -270,9 +270,9 @@ export default function Profile() {
                 >
                   Delete
                 </button>
-                {/* <Link to={`/update-listing/${listing._id}`}> */}
-                <button className="text-green-700 uppercase">Edit</button>
-                {/* </Link> */}
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
